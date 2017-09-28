75afb67d7e6278d47ff978af845e5875bf9af61d
bpo106 <bpo106bkv@gmail.com>
2017-09-28 11:18:04 +0200

    Print usage done

---
+++ TodoApp/TodoApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodoApp
+{
+    class Program
+    {
+        static void Inform()
+        {
+            Console.WriteLine("Command Line Todo application\n=============================\n\nCommand line arguments:\n -l   Lists all the tasks\n -a   Adds a new task\n -r   Removes an task\n -c   Completes an task");
+        }
+
+        static void Main(string[] args)
+        {
+            Inform();
+            Console.ReadLine();
+        }
+    }
+}
